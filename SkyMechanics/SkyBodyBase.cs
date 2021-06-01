39f2b1bcbf6e76e419f54dcdfa04c7fce0677bf1
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-06-01 16:49:51 +0300

    Description for SkyBodyBase.

--- SkyMechanics/SkyBodyBase.cs
+++ SkyMechanics/SkyBodyBase.cs
@@ -7,13 +7,39 @@ using System.Numerics;
 
 namespace SkyMechanics
 {
+    /// <summary>
+    /// Класс представляющий общие свойства небесного тела.
+    /// </summary>
     class SkyBodyBase
     {
+        /// <summary>
+        /// Радиус.
+        /// </summary>
         public float R { get; set; }
+
+        /// <summary>
+        /// Масса.
+        /// </summary>
+        public float Weight { get; set; }
+
+        /// <summary>
+        /// Позиция.
+        /// </summary>
         public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Скорость.
+        /// </summary>
         public Vector2 Velocity { get; set; }
+
+        /// <summary>
+        /// Ускорение.
+        /// </summary>
         public Vector2 Acceleration { get; set; }
 
+        /// <summary>
+        /// Движение.
+        /// </summary>
         public void Move()
         {
             Velocity += Acceleration;
