a15ccf55481aa09c19cee8b2d7df45a382886536
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-06-01 16:45:51 +0300

    Added  class SkyBodyBase

---
+++ SkyMechanics/SkyBodyBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace SkyMechanics
+{
+    class SkyBodyBase
+    {
+        public float R { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Vector2 Acceleration { get; set; }
+
+        public void Move()
+        {
+            Velocity += Acceleration;
+            Position += Velocity;
+            Acceleration = Vector2.Zero;
+        }
+    }
+}
